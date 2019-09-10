15e5a44a202ad162eb69067e759578c21c55ad36
Viachaslau Pashaliuk <v.pashaliuk@gmail.com>
2019-09-10 05:03:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
 # Your Code Here
+def map(source_array)
+  new_array = []
+  source_array.each{|num| new_array << yield(num)}
+  new_array
+end
+
+def reduce(source_array, starting_point = nil)
+  if starting_point
+    answer = starting_point
+  else
+    answer = source_array[0]
+  end
+  source_array.each{|element| answer = yield(answer, element)}
+  answer
+end
